12b70732326eea15b19ef0c30b7a2f8d2f97739e
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-12-13 13:31:01 +0100

    Small refacto day13

--- day13/solution_day13.py
+++ day13/solution_day13.py
@@ -29,14 +29,14 @@ def bezout_aux(r, u, v, rp, up, vp):
     if rp == 0:
         return (r, u, v)
     else:
-        aux = int(r / rp)
+        div = int(r / rp)
         return bezout_aux(
             rp,
             up,
             vp,
-            r - aux * rp,
-            u - aux * up,
-            v - aux * vp
+            r - div * rp,
+            u - div * up,
+            v - div * vp
         )
 
 
@@ -46,34 +46,34 @@ def bezout(a, b):
 
 def get_aligned_waiting_times(bus_times, expected_waittimes):
     # Bus arriving "modulo" minutes before n
+    # If bus {bus_times[i]} arrives {modulos[i]} minutes before n,
+    # the bus will also be arriving at {expected_waittimes[i]}
     modulos = bus_times - expected_waittimes % bus_times
     for i, _ in enumerate(bus_times):
-        print(
-            f'If bus {bus_times[i]} arrives {modulos[i]} minutes before n,'
-            f'the bus will also be arriving at {expected_waittimes[i]}'
-        )
-    prod = int(bus_times[0])
-    solution = int(modulos[0])
-    for i, bt in enumerate(bus_times[1:]):
-        # numpy.int64 leads to overflow
-        bt = int(bt)
-        new_prod = prod * bt
-        s = bezout(prod, bt)
-        print(s[0])
-        print(type(solution))
-        solution = (solution * s[2] * bt + int(modulos[i+1]) * s[1] * prod) % new_prod
-        prod = new_prod
-    return solution + 1 # Dunno why I systematically underestimate the solution by 1
+        prod = int(bus_times[0])
+        solution = int(modulos[0])
+        for i, bt in enumerate(bus_times[1:]):
+            # numpy.int64 leads to overflow
+            bt = int(bt)
+            new_prod = prod * bt
+            s = bezout(prod, bt)
+            solution = (solution * s[2] * bt +
+                        int(modulos[i+1]) * s[1] * prod) % new_prod
+            prod = new_prod
+    # Dunno why I systematically underestimate the solution by 1
+    return solution + 1
 
 
 if __name__ == "__main__":
     timestamp = 1000052
     bt = read_bus_times("./day13/input.txt")
     solution13A = first_available_bus(timestamp, bt)
+    print("Solution 13A:")
     print(solution13A[0] * solution13A[1])
     print()
 
     path = "./day13/input.txt"
     bt = read_bus_times(path)
     exp = read_expected_waiting_times(path)
+    print("Solution 13B:")
     print(get_aligned_waiting_times(bt, exp))
